aa10939df13c7e79f42ebd8877c17fb09355547b
Jae <dezcao@gmail.com>
2021-09-21 21:46:04 +0900

    fix: [로그인] 더미데이터 사용하도록 수정

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -35,8 +35,10 @@ const actions = {
     return new Promise((resolve, reject) => {
       login({ username: username.trim(), password: password }).then(response => {
         const { data } = response
-        commit('SET_TOKEN', data.token)
-        setToken(data.token)
+        commit('SET_TOKEN', 'admin-token')
+        setToken('admin-token')
+        // commit('SET_TOKEN', data.token)
+        // setToken(data.token)
         resolve()
       }).catch(error => {
         reject(error)
@@ -48,8 +50,13 @@ const actions = {
   getInfo({ commit, state }) {
     return new Promise((resolve, reject) => {
       getInfo(state.token).then(response => {
-        const { data } = response
-
+        // const { data } = response
+        let data = {
+          roles: ['admin'],
+          introduction: 'I am a super administrator',
+          avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+          name: 'Super Admin'
+        }
         if (!data) {
           reject('Verification failed, please Login again.')
         }
